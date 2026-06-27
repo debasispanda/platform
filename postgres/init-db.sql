fe844a884bd4a79179437033f714e203803044fd
Debasis Panda <8438038+debasispanda@users.noreply.github.com>
2026-06-27 22:45:52 +0530

    feat: configure traefik and keycloak behind it

--- postgres/init-db.sql
+++ postgres/init-db.sql
@@ -2,4 +2,4 @@
 -- All the keyloak tables will be created under this schema
 
 CREATE SCHEMA IF NOT EXISTS keycloak AUTHORIZATION postgres;
-CREATE SCHEMA IF NOT EXISTS platform AUTHORIZATION postgres;
+CREATE SCHEMA IF NOT EXISTS ${PLATFORM_SCHEMA_NAME} AUTHORIZATION postgres;
